c8aac12484409ae91616b1dde1b12b8c8aa1ed80
Misael812 <misaelnava812@gmail.com>
2023-09-13 10:23:56 -0600

    Edited ini file, when running on windows add /bin/renode to work. Fix small errors so that the three tests would pass

--- lib/blink/src/PracticeRefactor.c
+++ lib/blink/src/PracticeRefactor.c
@@ -1,11 +1,11 @@
 #include "PracticeRefactor.h"
 
-void toggle_led(const struct device* dev, const gpio_pin_t pin, bool* state) {
+void toggle_led(const struct device* dev, gpio_pin_t pin, bool* state) {
     gpio_pin_set(dev, pin, (int)(*state));
     *state = !(*state);
 }
 
-void toggle_thread_led(const struct device* dev, const gpio_pin_t pin, bool* state, int* count) {
+void toggle_thread_led(const struct device* dev, gpio_pin_t pin, bool* state, int* count) {
     *count = *count + 1;
     gpio_pin_set(dev, pin, (int)(*state));
     *state = !(*state);
